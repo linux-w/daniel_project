ecd6fe10cd111f5a2743689eb1672776669bad9e
admin <daniel@itcast.cn>
2019-03-26 11:25:16 +0800

    keliong

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
+#!/usr/bin/python
+#!/usr/bin/python
+#!/usr/bin/python
+#!/usr/bin/python
+#!/usr/bin/python
 
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
+print("hello world)
 print("hello world)
